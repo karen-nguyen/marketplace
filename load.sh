ca781af030e4448a094f8a7c6f3948ea017c1a3e
karen <karen.nguyen@accantosystems.com>
2020-02-18 11:02:09 +0200

    Comment out unused resources in load.sh

--- load.sh
+++ load.sh
@@ -1,22 +1,22 @@
 cd vnfs/ip-pbx
 lmctl project $1 $2
-cd ../vyos
-lmctl project $1 $2
+#cd ../vyos
+#lmctl project $1 $2
 cd ../voip-gateway
 lmctl project $1 $2
 cd ../sip-performance
 lmctl project $1 $2
 cd ../../network-services/mgmt_infra
 lmctl project $1 $2
-cd ../../network-services/mgmt_infra_provider
-lmctl project $1 $2
+#cd ../../network-services/mgmt_infra_provider
+#lmctl project $1 $2
 cd ../voice-service
 lmctl project $1 $2
-cd ../voice-service-provider
-lmctl project $1 $2
+#cd ../voice-service-provider
+#lmctl project $1 $2
 cd ../voice-load-generator
 lmctl project $1 $2
-cd ../voice-load-generator-provider
-lmctl project $1 $2
-cd ../vpn
-lmctl project $1 $2
+#cd ../voice-load-generator-provider
+#lmctl project $1 $2
+#cd ../vpn
+#lmctl project $1 $2
